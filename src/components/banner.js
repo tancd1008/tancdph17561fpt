55de09222a77e977e1524639ab0253241fb133eb
tancd <caotan1008@gmail.com>
2022-02-22 10:05:15 +0700

    banner swiper

---
+++ src/components/banner.js
@@ -0,0 +1,43 @@
+/* eslint-disable import/no-unresolved */
+// import Swiper JS
+import Swiper from "swiper/bundle";
+import "swiper/css/bundle";
+
+const Banner = {
+    render() {
+        return /* html */ `
+        <div class="swiper">
+            <!-- Additional required wrapper -->
+            <div class="swiper-wrapper">
+                <!-- Slides -->
+                <div class="swiper-slide">
+                    <img src="https://picsum.photos/1024/400" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="https://picsum.photos/1024/400" />    
+                </div>
+                <div class="swiper-slide">
+                    <img src="https://picsum.photos/1024/400" />    
+                </div>
+            </div>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-pagination"></div>
+        </div>
+        `;
+    },
+    afterRender() {
+        // eslint-disable-next-line no-unused-vars
+        const swiper = new Swiper(".swiper", {
+            pagination: {
+                el: ".swiper-pagination",
+                type: "progressbar",
+            },
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+        });
+    },
+};
+export default Banner;
